a503bc0d4898c8d53825b437c6675a0db677ec92
Julius Seporaitis <julius@seporaitis.net>
2019-05-01 23:57:13 +0100

    Add a basic preact component.

--- static_source/src/dashboard.js
+++ static_source/src/dashboard.js
@@ -1,1 +1,30 @@
-console.log('Hello dashboards!');
+import { h, render, Component } from 'preact';
+
+/** jsx h */
+class TodoList extends Component {
+  state = { todos: [], text: '' };
+
+  setText = e => {
+    this.setState({text: e.target.value});
+  };
+
+  addTodo = () => {
+    let { todos, text } = this.state;
+    todos = todos.concat({text});
+    this.setState({todos, text: ''});
+  }
+
+  render({ }, { todos, text }) {
+    return (
+      <form onSubmit={this.addTodo} action="javascript:">
+        <input value={text} onInput={this.setText} />
+        <button type="submit">Add</button>
+        <ul>
+        { todos.map( todo => (<li>{todo.text}</li>)) }
+        </ul>
+      </form>
+    );
+  }
+}
+
+render(<TodoList/>, document.getElementById('component-dashboard'));
